38e27fdb8b9f43b8ddf03329c70aee9a8edd259f
pibcrib <34898247+pibcrib@users.noreply.github.com>
2021-10-05 22:51:48 -0400

    update changes

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -9,8 +9,8 @@ database_name = "castingagency"
 
 #default path: postgresql://postgres@localhost:5432/castingagency
 #database_path = os.environ.get("DATABASE_URL", "postgresql://postgres@{}/{}".format('localhost:5432', database_name))
-
-database_path = "postgresql://rzgycskfuglwnp:8ad0e6c8ec5f339f1eda0de9015446930f742236fb019509455f9dfb3f2958eb@ec2-44-193-150-214.compute-1.amazonaws.com:5432/dfk5qivnqiqkh1"
+#database_path = "postgresql://rzgycskfuglwnp:8ad0e6c8ec5f339f1eda0de9015446930f742236fb019509455f9dfb3f2958eb@ec2-44-193-150-214.compute-1.amazonaws.com:5432/dfk5qivnqiqkh1"
+database_path = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
 
 db = SQLAlchemy()
 
